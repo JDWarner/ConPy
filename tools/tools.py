393217e4c408f74e0e60e95ce87eaf288ae741ec
Robert Unguran <unguranr@gmail.com>
2012-04-13 19:08:59 +0200

    tools

---
+++ tools/tools.py
@@ -0,0 +1,32 @@
+__author__ = "Robert Unguran"
+__copyright__ = "Copyright 2012"
+__credits__ = ["Robert Unguran"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Robert Unguran"
+__email__ = "unguranr@gmail.com"
+__status__ = "Under Development"
+
+from numpy import hstack , vstack
+
+def c2d ( sys , Ts, method = 'ZOH' ) :
+	"""
+	From continous time convert discrete time
+	"""
+	if(len(sys) == 2):
+		(A, B, C, D)	=	tf2ss( sys )
+	else:
+		(A, B, C, D)	=	sys
+	
+	n	=	A.shape[ 0 ]
+	nb	=	B.shape[ 1 ]
+	ztmp=	zeros ( ( nb , n + nb ) )
+	tmp	=	hstack ( ( a , b ) )
+	tmp	=	vstack ( ( tmp , ztmp ) )
+	tmp	=	expm( tmp * Ts )
+	A	=	tmp [ 0 : n , 0 : n ]
+	B	=	tmp [ 0 : n , n : n+nb ]
+	sysd=	ss( A , B , C , D , Ts )
+	if(len(sys) == 2):
+		return ss2tf ( s y s d )
+	return sysd
